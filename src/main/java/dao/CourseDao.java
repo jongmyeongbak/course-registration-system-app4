ac4ed8f534404fdcdbb57ec10e92cb6870230cf9
jongmyeongbak <withpleasure@gmail.com>
2023-05-28 16:56:55 +0900

    feat: Professor course list

--- src/main/java/dao/CourseDao.java
+++ src/main/java/dao/CourseDao.java
@@ -24,6 +24,11 @@ public class CourseDao {
 			return rs.getInt("cnt");
 		});
 	}
+	public int getTotalRowsByProfessorId(String professorId) {
+		return DaoHelper.selectOne("courseDao.getTotalRowsByProfessorId", rs -> {
+			return rs.getInt("cnt");
+		}, professorId);
+	}
 	
 	public List<Course> getCourses(int first, int last) {
 		return DaoHelper.selectList("courseDao.getCourses", rs -> {
@@ -31,9 +36,20 @@ public class CourseDao {
 			course.setName(rs.getString("course_name"));
 			course.setQuota(rs.getInt("course_quota"));
 			course.setReqCnt(rs.getInt("course_req_cnt"));
-			course.setDept(new Dept(rs.getInt("dept_no"), rs.getString("dept_name")));
+			course.setDept(new Dept(0, rs.getString("dept_name")));
 			course.setProfessor(new Professor(null, rs.getString("professor_name")));
 			return course;
 		}, first, last);
+	}
+	public List<Course> getCoursesByProfessorId(String professorId, int first, int last) {
+		return DaoHelper.selectList("courseDao.getCoursesByProfessorId", rs -> {
+			Course course = new Course(rs.getInt("course_no"));
+			course.setName(rs.getString("course_name"));
+			course.setType(rs.getString("course_type"));
+			course.setQuota(rs.getInt("course_quota"));
+			course.setReqCnt(rs.getInt("course_req_cnt"));
+			course.setDept(new Dept(0, rs.getString("dept_name")));
+			return course;
+		}, professorId, first, last);
 	}
 }
